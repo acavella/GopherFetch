598208cd22756e81e4c9a0d2d7e052d8bc76b5bd
Tony Cavella <9434191+acavella@users.noreply.github.com>
2026-03-02 20:11:05 -0500

    initial commit

--- main.go
+++ main.go
@@ -1,176 +1,133 @@
 package main
 
 import (
-	"crypto/x509"
-	"errors"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
-	"os/signal"
 	"path/filepath"
-	"strings"
-	"syscall"
-	"time"
+	"sync"
 
-	"github.com/spf13/viper"
-
-	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v3"
 )
 
-var workpath = "/usr/local/bin/gorevoke"
+type Config struct {
+	DownloadDir string `yaml:"download_directory"`
+	Files       []File `yaml:"files"`
+}
 
-var appVersion = "0.0.0"
-var appBuild = "UNK"
-var appBuildDate = "00000000-0000"
-
-func init() {
-	log.SetFormatter(&log.TextFormatter{
-		QuoteEmptyFields: true,
-		FullTimestamp:    true,
-	})
-
-	printver()
-
-	directory, err := filepath.Abs(filepath.Dir(os.Args[0])) //get the current working directory
-	if err != nil {
-		log.Fatal(err) //print the error if obtained
-	}
-
-	workpath = directory // set app working directory
-
-	// Set config file paths
-	viper.SetConfigName("gorevoke")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath(".")
-	viper.AddConfigPath("$HOME/.gorevoke/")
-	viper.AddConfigPath("/etc/")
-
-	// Set defaults
-	viper.SetDefault("default.interval", 5)
-	viper.SetDefault("default.webserver", false)
-	viper.SetDefault("default.port", 4000)
-	viper.SetDefault("default.crldir", workpath + "/crl")
-
-	// Enable environment variable configuration
-	viper.AutomaticEnv()
-	viper.SetEnvPrefix("gorevoke")
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-
-	// Load config file
-	if configErr := viper.ReadInConfig(); configErr != nil {
-		if _, ok := configErr.(viper.ConfigFileNotFoundError); ok {
-			// Config file not found
-			log.Warn("no config file found, using defaults/environment")
-		} else {
-			// Config file was found but another error was produced
-			panic(fmt.Errorf("fatal error reading config file: %w", configErr))
-		}
-	}
-
-	// Initialize temp dir if not set
-	if !viper.IsSet("default.tmpdir") {
-		tmpdir, err := os.MkdirTemp("", "gorevoke-*")
-		if err != nil {
-			panic(fmt.Errorf("error creating temporary directory: %w", err))
-		}
-		viper.Set("default.tmpdir", tmpdir)
-		log.Info("Created temp directory for CRLs: ", tmpdir)
-	}
+type File struct {
+	URL      string `yaml:"url"`
+	Filename string `yaml:"filename"`
 }
 
 func main() {
-	// Catch SIGTERM in order to cleanup temp files
-    c := make(chan os.Signal)
-    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-    go func() {
-        <-c
-        cleanup()
-        os.Exit(1)
-    }()
-
-	// Retrieve config values
-	crls := viper.GetStringMapString("crls")
-	refresh := viper.GetInt("default.interval")
-	webport := viper.GetString("default.port")
-	server := viper.GetBool("default.webserver")
-	tmpdir := viper.GetString("default.tmpdir")
-	crldir := viper.GetString("default.crldir")
-
-	if server {
-		go webserver(webport)
-	}
-
-	log.Info("CRLs in list: ", len(crls))
-	log.Info("Refresh interval: ", time.Duration(int(time.Second)*int(refresh)))
-
-	for {
-		for caId, caUrl := range crls {
-			var tmpfile string = tmpdir + "/" + caId + ".crl"
-			var httpfile string = crldir + "/" + caId + ".crl"
-
-			DownloadFile(tmpfile, caUrl) // Download CRL from remote
-
-			crlfile, err := os.ReadFile(tmpfile)
-			if err != nil {
-				log.Error("Problem opening downloaded file: ", err)
-				log.Info("Moving to next CRL entry.")
-				continue
-			} else {
-				crl, err := x509.ParseRevocationList(crlfile)
-				if err != nil {
-					log.Errorln("Skipping CRL: ", err)
-					continue
-				} else {
-					log.Infof("CRL %s is valid, issued by %s", caId, crl.Issuer.CommonName)
-				}
-			}
-
-			if _, err := os.Stat(httpfile); err == nil {
-				// file exists
-				log.Info("CRL already exists")
-				h1, err := getHash(tmpfile)
-				if err != nil {
-					log.Error("Error hashing: ", err)
-				}
-				h2, err2 := getHash(httpfile)
-				if err2 != nil {
-					log.Error("Error hashing: ", err2)
-				}
-				log.Debug(h1, h2, h1 == h2)
-				if h1 != h2 {
-					log.Info("File hashes do not match: ", h1, h2)
-					log.Info("Copying file to destination: ", httpfile)
-					copy(tmpfile, httpfile)
-				} else {
-					log.Info("No changes detected, proceeding.")
-				}
-			} else if errors.Is(err, os.ErrNotExist) {
-				// file does not exist
-				log.Info("CRL is new, copying to: ", httpfile)
-				bytes, err := copy(tmpfile, httpfile)
-				if err != nil {
-					log.Errorf("Error copying downloaded CRL to destination %s: %s", httpfile, err)
-				}
-				log.Debugf("Copied %d bytes from %s to %s", bytes, tmpfile, httpfile)
-			} else {
-				// catch anything else
-				return
-			}
-		}
-		time.Sleep(time.Duration(int(time.Second) * refresh)) // Defines time to sleep before repeating
-	}
-}
-
-func cleanup() {
-	tmpdir := viper.GetString("default.tmpdir")
-	log.Infof("Shutdown signal received, removing temp directory %s", tmpdir)
-	err := os.RemoveAll(tmpdir)
+	yamlFile, err := os.ReadFile("config.yaml")
 	if err != nil {
-		log.Errorf("Could not remove %s: %s", tmpdir, err)
+		log.Fatalf("Error reading config.yaml: %v", err)
 	}
+
+	var config Config
+	if err = yaml.Unmarshal(yamlFile, &config); err != nil {
+		log.Fatalf("Error parsing YAML: %v", err)
+	}
+
+	if err = os.MkdirAll(config.DownloadDir, os.ModePerm); err != nil {
+		log.Fatalf("Error creating download directory: %v", err)
+	}
+
+	var wg sync.WaitGroup
+
+	for _, file := range config.Files {
+		wg.Add(1)
+		destPath := filepath.Join(config.DownloadDir, file.Filename)
+		
+		// Pass the DownloadDir as well so we can create temp files on the same drive
+		go downloadAndVerifyFile(file.URL, destPath, config.DownloadDir, &wg)
+	}
+
+	wg.Wait()
+	fmt.Println("All downloads processed successfully.")
 }
 
-func printver() {
-	fmt.Printf("GoRevoke ver. %s\n", appVersion)
-	fmt.Printf("Build Type: %s\n", appBuild)
-	fmt.Printf("Build Date: %s\n", appBuildDate)
+// downloadAndVerifyFile handles fetching, hashing, and replacing the file if necessary
+func downloadAndVerifyFile(url, destPath, destDir string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Printf("Checking %s...\n", destPath)
+
+	// 1. Create a temporary file in the destination directory
+	tempFile, err := os.CreateTemp(destDir, "temp-dl-*")
+	if err != nil {
+		log.Printf("Failed to create temp file for %s: %v\n", url, err)
+		return
+	}
+	tempName := tempFile.Name()
+	
+	// Ensure the temp file is cleaned up if we exit early or after renaming
+	defer os.Remove(tempName) 
+
+	// 2. Download the file into the temporary file
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf("Failed to download %s: %v\n", url, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Bad status: %s for URL: %s\n", resp.Status, url)
+		return
+	}
+
+	if _, err = io.Copy(tempFile, resp.Body); err != nil {
+		log.Printf("Failed to write data to temp file for %s: %v\n", url, err)
+		return
+	}
+	
+	// We must close the temp file before we can read it to hash it, or rename it (especially on Windows)
+	tempFile.Close() 
+
+	// 3. Calculate the hash of the newly downloaded file
+	newHash, err := hashFile(tempName)
+	if err != nil {
+		log.Printf("Failed to hash temp file for %s: %v\n", url, err)
+		return
+	}
+
+	// 4. Check if the target file exists and compare hashes
+	if _, err := os.Stat(destPath); err == nil {
+		existingHash, err := hashFile(destPath)
+		if err == nil && newHash == existingHash {
+			fmt.Printf("Skipped: %s (Identical file already exists)\n", destPath)
+			return // The defer os.Remove will silently clean up the temp file
+		}
+	}
+
+	// 5. Move the temp file to the final destination (overwriting if it exists but differs)
+	if err := os.Rename(tempName, destPath); err != nil {
+		log.Printf("Failed to move temp file to %s: %v\n", destPath, err)
+		return
+	}
+
+	fmt.Printf("Successfully updated/downloaded: %s\n", destPath)
+}
+
+// hashFile is a helper function that generates a SHA-256 hash string for a given file
+func hashFile(filePath string) (string, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	hasher := sha256.New()
+	if _, err := io.Copy(hasher, f); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(hasher.Sum(nil)), nil
 }
